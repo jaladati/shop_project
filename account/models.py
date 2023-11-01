734d8a5f587ca276654f66633f4e8a655f394a9a
ali-jaladati <ali.jaladati0@gmail.com>
2023-11-01 23:31:02 +0330

    my custom user model was created and the project language was changed to Farsi

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to="images/profiles", null=True, blank=True, verbose_name="آواتار")
+    email_activate_code = models.CharField(max_length=100, verbose_name="کد فعالسازی ایمیل")
+    is_active = models.BooleanField(default=False, verbose_name="فعال")
+    address = models.TextField(verbose_name="آدرس", blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.get_full_name() if self.get_full_name() else self.username
--- shop_project/settings.py
+++ shop_project/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'account.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_ROOT = BASE_DIR / 'medias'
+MEDIA_URL = '/uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
