f7794fc722fbfd26936df9681b397cff5491a3f2
ali-jaladati <ali.jaladati0@gmail.com>
2023-11-01 23:54:55 +0330

    create user admin and meta class for user model

--- account/models.py
+++ account/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to="images/profiles", null=True, blank=True, verbose_name="آواتار")
-    email_activate_code = models.CharField(max_length=100, verbose_name="کد فعالسازی ایمیل")
+    avatar = models.ImageField(
+        upload_to="images/profiles", null=True, blank=True, verbose_name="آواتار")
+    email_activate_code = models.CharField(
+        max_length=100, verbose_name="کد فعالسازی ایمیل")
     is_active = models.BooleanField(default=False, verbose_name="فعال")
     address = models.TextField(verbose_name="آدرس", blank=True, null=True)
-    
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
+
     def __str__(self) -> str:
         return self.get_full_name() if self.get_full_name() else self.username
