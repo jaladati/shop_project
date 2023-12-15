999423c2a2b5fd03c3afbbf99b59aabd7009b5ee
ali-jaladati <ali.jaladati0@gmail.com>
2023-12-15 22:31:40 +0330

    fix bogs

--- product/models.py
+++ product/models.py
@@ -12,8 +12,6 @@ class EnabledManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_enable=True)
 
-# TODO: makemigrations and migrate then complete the latest products and commit and finally move to linux.
-
 
 class Category(models.Model):
     title = models.CharField(max_length=255, verbose_name="عنوان")
@@ -39,34 +37,6 @@ class Category(models.Model):
         return super().save(*args, **kwargs)
 
 
-class ProductColorVariant(models.Model):
-    COLOR_PALETTE = [
-        ("#000000", "سیاه"),
-        ("#FFFFFF", "سفید"),
-        ("#FF0000", "قرمز"),
-        ("#0000FF", "آبی"),
-        ("#008000", "سبز"),
-        ("#FFFF00", "زرد"),
-        ("#FFA500", "نارنجی"),
-        ("#800080", "بنفش"),
-        ("#FFC0CB", "صورتی"),
-        ("#964B00", "قهوه ای"),
-        ("#808080", "طوسی"),
-    ]
-    color_name = models.CharField(
-        unique=True, max_length=300, verbose_name="نام رنگ")
-    color_hex_code = ColorField(
-        samples=COLOR_PALETTE, verbose_name="کد hex رنگ")
-    stock_count = models.PositiveBigIntegerField(verbose_name="تعداد موجودی")
-
-    class Meta:
-        verbose_name = "رنگ"
-        verbose_name_plural = "رنگ ها"
-
-    def __str__(self) -> str:
-        return F"{self.color_name}-{self.stock_count}"
-
-
 class Product(models.Model):
     title = models.CharField(max_length=255, verbose_name="عنوان")
     slug = models.SlugField(max_length=300, blank=True, verbose_name="اسلاگ")
@@ -74,7 +44,7 @@ class Product(models.Model):
 
     price = models.PositiveBigIntegerField(verbose_name="قیمت")
     off = models.DecimalField(
-        max_digits=6, decimal_places=2, default=0, validators=[MaxValueValidator(100.0), MinValueValidator(0.0)], verbose_name="تخفیف")
+        max_digits=5, decimal_places=1, default=0, validators=[MaxValueValidator(100.0), MinValueValidator(0.0)], verbose_name="تخفیف")
 
     created_time = jmodels.jDateTimeField(
         auto_now_add=True, verbose_name="تاریخ ایجاد")
@@ -88,11 +58,9 @@ class Product(models.Model):
 
     size = models.CharField(max_length=100, null=True,
                             blank=True, verbose_name="اندازه")
-    color_variant = models.ManyToManyField(
-        to=ProductColorVariant, related_name="products")
 
     image = models.ImageField(
-        upload_to="images/products", null=True, blank=True, verbose_name="تصویر")
+        upload_to="images/products", verbose_name="تصویر")
 
     objects = jmodels.jManager()
     enabled = EnabledManager()
@@ -114,11 +82,44 @@ class Product(models.Model):
 
     @property
     def stock_count(self):
-        return self.color_variant.all().aggregate(Sum("stock_count"))['stock_count__sum']
+        return self.color_variants.all().aggregate(Sum("stock_count"))["stock_count__sum"]
 
     @property
     def final_price(self):
-        return self.price - self.price * self.off / 100
+        return int(self.price - self.price * self.off / 100)
+
+
+class ProductColorVariant(models.Model):
+    COLOR_PALETTE = [
+        ("#000000", "سیاه"),
+        ("#FFFFFF", "سفید"),
+        ("#FF0000", "قرمز"),
+        ("#0000FF", "آبی"),
+        ("#008000", "سبز"),
+        ("#FFFF00", "زرد"),
+        ("#FFA500", "نارنجی"),
+        ("#800080", "بنفش"),
+        ("#FFC0CB", "صورتی"),
+        ("#964B00", "قهوه ای"),
+        ("#808080", "طوسی"),
+    ]
+
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE,
+                                related_name="color_variants", verbose_name="محصول")
+
+
+    color_name = models.CharField(
+        max_length=300, verbose_name="نام رنگ")
+    color_hex_code = ColorField(
+        samples=COLOR_PALETTE, verbose_name="کد hex رنگ")
+    stock_count = models.PositiveBigIntegerField(verbose_name="تعداد موجودی")
+
+    class Meta:
+        verbose_name = "رنگ"
+        verbose_name_plural = "رنگ ها"
+
+    def __str__(self) -> str:
+        return F"{self.color_name}-{self.stock_count}"
 
 
 class ProductGallery(models.Model):
