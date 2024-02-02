523007e335864b85c7089dce7a20394f4d207c58
ali-jaladati <ali.jaladati0@gmail.com>
2024-02-02 21:20:14 +0330

    some email sevices

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -142,3 +142,13 @@ MEDIA_URL = '/uploads/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
