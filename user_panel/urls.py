c2dd81db3f5a1d1d6765b48bcd697143eb40ef3e
ali-jaladati <ali.jaladati0@gmail.com>
2024-03-20 02:31:29 +0330

    Create the user's custom lists pages and add the wishlist to it

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import DashboardView, EditProfileView, cart, change_password
+from .views import (
+    DashboardView,
+    EditProfileView,
+    cart,
+    change_password,
+    lists,
+    list_detail,
+    products_list_filter
+)
 
 
 app_name = 'user_panel'
@@ -9,5 +17,8 @@ urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("edit-profile/", EditProfileView.as_view(), name="edit_profile"),
     path("change-password/", change_password, name="change_password"),
+    path("lists/", lists, name="lists"),
+    path("lists/filter/", products_list_filter, name="filter"),
+    path("lists/<str:list_title>/", list_detail, name="list_detail"),
     path("cart/", cart, name="cart")
 ]
--- user_panel/views.py
+++ user_panel/views.py
@@ -1,11 +1,19 @@
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.views import View
 from django.views.generic import UpdateView
-from django.shortcuts import redirect, render
+from django.views.decorators.http import require_GET
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
+from django.core.paginator import (
+    Paginator,
+    PageNotAnInteger,
+    EmptyPage
+)
 
 from account import models
 from cart.models import Cart
@@ -28,10 +36,10 @@ class EditProfileView(UpdateView):
     template_name = "user_panel/edit_profile.html"
     success_url = reverse_lazy("user_panel:dashboard")
 
-    def get_object(self, queryset = ...):
+    def get_object(self, queryset=...):
         user: models.User = self.request.user
         return self.model.objects.get(is_active=True, id=user.id)
-    
+
     def form_valid(self, form) -> HttpResponse:
         messages.success(self.request, "اطلاعات شما با موفقیت تغییر یافت")
         return super().form_valid(form)
@@ -53,6 +61,7 @@ def change_password(request):
             if user.check_password(cd["current_password"]):
                 user.set_password(cd["password"])
                 user.save()
+                login(request, user)
                 messages.success(request, "گذرواژه شما با موفقیت تغییر یافت")
                 return redirect(reverse("user_panel:dashboard"))
             else:
@@ -61,13 +70,86 @@ def change_password(request):
         return render(request, "user_panel/change_password.html", context)
 
 
+@require_GET
+@login_required
+def lists(request):
+    """User lists page"""
+    # TODO: add user's custom lists.
+    return render(request, "user_panel/lists.html")
+
+
+def base_product_list_filter(request, list_title):
+    # Get user.
+    user = request.user
+    if list_title == "لیست آرزو":
+        # Get the user's wish list products.
+        products = user.liked_products.all()
+    else:
+        # TODO: get the list from custom lists of user.
+        products = user.liked_products.all()
+
+    # Paginate based on user selection.
+    paginate_by = request.GET.get("paginate_by", "10")
+    if paginate_by not in ["5", "10", "15"]:
+        paginate_by = 10
+
+    # Create the paginator object.
+    paginator = Paginator(products, paginate_by)
+
+    # Get the page number.
+    page = request.GET.get("page", 1)
+
+    # Get the selected page.
+    try:
+        page_obj = paginator.get_page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        page_obj = paginator.get_page(paginator.num_pages)
+
+    return page_obj, paginate_by
+
+
+@require_GET
+@login_required
+def list_detail(request, list_title):
+    page_obj, _ = base_product_list_filter(request, list_title)
+    context = {
+        "page_obj": page_obj,
+        "list_title": list_title
+    }
+    return render(request, "user_panel/list_detail.html", context)
+
+
+@require_GET
+@login_required
+def products_list_filter(request) -> JsonResponse:
+    """Filter the product list with ajax"""
+    list_title = request.GET.get("list_name")
+    page_obj, paginate_by = base_product_list_filter(
+        request, list_title)
+
+    data = {}
+    products_filter_url = reverse("user_panel:filter")
+    if request.GET.get("paginate_change") == "true":
+        data["paginates"] = render_to_string(
+            "components/paginate.html", {"paginate_by": paginate_by, "products_filter_url": products_filter_url})
+    data["products"] = render_to_string(
+        "includes/products_partial.html", {"products": page_obj.object_list}, request)
+    data["pagination"] = render_to_string(
+        "components/paging.html", {"page_obj": page_obj, "products_filter_url": products_filter_url})
+
+    return JsonResponse(data)
+
+
+@require_GET
 @login_required
 def cart(request):
-    if request.method == "GET":
-        user = request.user
-        cart, _ = Cart.objects.prefetch_related("items").get_or_create(user=user, is_paid=False)
-        
-        context = {
-            "cart": cart
-        }
-        return render(request, "user_panel/cart.html", context)
+    user = request.user
+    cart, _ = Cart.objects.prefetch_related(
+        "items").get_or_create(user=user, is_paid=False)
+
+    context = {
+        "cart": cart
+    }
+    return render(request, "user_panel/cart.html", context)
