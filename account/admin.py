6983a650adff15613ea6c86e91dbcd29362e0343
ali-jaladati <ali.jaladati0@gmail.com>
2024-05-05 02:22:48 +0330

    sample settings

--- account/admin.py
+++ account/admin.py
@@ -6,5 +6,6 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "first_name", "last_name", "is_active"]
+    list_editable = ["is_active"]
     list_filter = ["is_staff", "is_superuser", "is_active"]
     search_fields = ["username", "email", "first_name", "last_name"]
